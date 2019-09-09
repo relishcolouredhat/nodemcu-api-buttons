f5fd4f8c9108b2a3e47f27155151a1ccc6a434ac
relishcolouredhat <klccomstock@gmail.com>
2019-09-08 23:50:20 -0400

    done broke it (10 min increment per second!)

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@ int y_out = 2;
 int g_in  = 16;
 int g_out = 14;
 int runCounter = 0;
-String version = "0.3.2";
+String version = "0.3.7";
 //int inputPins = {r_in,y_in,g_in}
 //int outputPins = {r_out,y_out,g_out}
 ESP8266WiFiMulti wlan;
@@ -55,6 +55,9 @@ void allToggle(){
     }
 }
 
+
+bool timeUnset = true;
+
 uint32_t getTime() {
   if (UDP.parsePacket() == 0) { // If there's no response (yet)
     return 0;
@@ -67,10 +70,21 @@ uint32_t getTime() {
   const uint32_t seventyYears = 2208988800UL;
   // subtract seventy years:
   uint32_t UNIXTime = NTPTime - seventyYears;
+  timeUnset = false;
   return UNIXTime;
 }
 
-void sendNTPpacket(IPAddress& address) {
+
+/*
+    function sendNTPpacket
+    Modified from stackoverflow
+    Input   :   - NTP Server IP
+                - NTP Port (default 123)
+    Mutates :   - Zeros NTPBuffer
+                - Fills buffer with NTP Request
+                - Sends UDP packet to address:port  */
+void sendNTPpacket(IPAddress& address, int port = 123) {
+  Serial.println("Sending NTP Packet!");
   memset(NTPBuffer, 0, NTP_PACKET_SIZE);  // set all bytes in the buffer to 0
   // Initialize values needed to form NTP request
   NTPBuffer[0] = 0b11100011;   // LI, Version, Mode
@@ -78,19 +92,24 @@ void sendNTPpacket(IPAddress& address) {
   UDP.beginPacket(address, 123); // NTP requests are to port 123
   UDP.write(NTPBuffer, NTP_PACKET_SIZE);
   UDP.endPacket();
-}
+  }
+
+uint32_t blockingGetTime(IPAddress& timeServerIP) {
+  sendNTPpacket(timeServerIP);
+  getTime();
+} // Blocking UDP time request which sleeps until time return by running sendNTPpacket() followed by getTime().
 
 inline int getSeconds(uint32_t UNIXTime) {
   return UNIXTime % 60;
-}
+} //returns modulo of input for seconds
 
 inline int getMinutes(uint32_t UNIXTime) {
   return UNIXTime / 60 % 60;
-}
+} //returns modulo of input for minutes
 
 inline int getHours(uint32_t UNIXTime) {
   return UNIXTime / 3600 % 24;
-}
+} //returns modulo of input for hours
 
 void startUDP() {
   Serial.println("Starting UDP");
@@ -100,7 +119,52 @@ void startUDP() {
   Serial.println();
 }
 
+unsigned long intervalNTP = 60000; // Request NTP time every minute
+//unsigned long prevNTP = 0;
+unsigned long lastNTPResponse = millis();
+unsigned long prevNTP = millis() - 120000; //(intervalNTP + 1);
+uint32_t timeUNIX = 0;
+unsigned long prevActualTime = 0;
+
+
+//int* setupNTP(
+//  resyncIntervalMillis=60000/
+//  resyncIntervalMillis=60000
+//
+//  )
+//}
+
+//uint32_t snycNTP()
+
+//uint32_t checkNTPudp
+
+
+/*
+  Input   : nil
+  Requires: NRP request to have been sent - this just checks for an answer
+  Output  : UNIXTime in ms?
+  Mutates : - Sets lastNTPResponse to current uptime
+*/
+uint32_t checkNTPtime(){
+  uint32_t time = getTime();                   // Check if an NTP response has arrived and get the (UNIX) time
+  if (time) {                                  // If a new timestamp has been received
+      unsigned long currentMillis = millis();
+      timeUNIX = time;
+      Serial.print("NTP response:\t");
+      Serial.println(timeUNIX);
+      lastNTPResponse = currentMillis;
+      return timeUNIX;
+    } else {
+      Serial.println("No NTP response found after checking!");
+      return 0;
+    }
+  }
+
+
+
+
 void setup() {
+
   int buttonCount = sizeof(buttonPins) / sizeof(buttonPins[0]);
   for (byte i = 0; i < buttonCount; i++){
     pinMode(buttonPins[i][0], INPUT);
@@ -126,6 +190,7 @@ void setup() {
   Serial.println("Adding wlan: NeoBadger...");
   wlan.addAP("NeoBadger","huemonsterventshiny");
   Serial.println("Trying to connect to wlan...");
+  espconn_dns_setserver(0, DNS_IP); //to set the primary DNS to 8.8.8.4
   wlan.run();
   String mdnsHandle;
   mdnsHandle = "test-hostname";
@@ -162,7 +227,10 @@ void setup() {
   Serial.println(timeServerIP);
 
   Serial.println("\r\nSending NTP request ...");
+  unsigned long currentMillis = millis();
   sendNTPpacket(timeServerIP);
+  //replace with func checkNTPtime()
+  timeUNIX = checkNTPtime();
 }
 
 void allLed(uint8_t instr){
@@ -257,17 +325,17 @@ void notifyGame(){
 }
 
 void printTakeoverTime(){
-  if (getTime() != 0 ){
+  if (timeUNIX != 0 ){
+    uint32_t currentTime = blockingGetTime(timeServerIP);
     //unsigned long currentMillis = millis();
     //sendNTPpacket(timeServerIP);
     //unsigned long lastNTPResponse = millis();
     //unsigned long prevNTP = millis() - 120000; //(intervalNTP + 1);
     //uint32_t timeUNIX = 0;
-    uint32_t actualTime = millis()/1000;
-    uint32_t futureTime = actualTime + 300; // + incrementSeconds
+    //uint32_t futureTime = time + 300; // + incrementSeconds
     //Serial.println("");
     //Serial.println("");
-    Serial.printf("\tTakover Time:\t2019-09-08T%.2d:%.2d:%.2d-00:00   \t", getHours(futureTime), getMinutes(futureTime), getSeconds(futureTime));
+    Serial.printf("\tTakover Time:\t2019-09-08T%.2d:%.2d:%.2d-00:00   \t", getHours(currentTime), getMinutes(currentTime), getSeconds(currentTime));
     //Serial.println("");
     //Serial.println("");
   } else {Serial.println("No NTP sync yet!");}
@@ -325,36 +393,28 @@ void buttonTest(){
     }
 }
 
-
-
-unsigned long intervalNTP = 60000; // Request NTP time every minute
-//unsigned long prevNTP = 0;
-unsigned long lastNTPResponse = millis();
-unsigned long prevNTP = millis() - 120000; //(intervalNTP + 1);
-uint32_t timeUNIX = 0;
-
-unsigned long prevActualTime = 0;
-
-void loop() {
-  unsigned long currentMillis = millis();
-  if (currentMillis - prevNTP > intervalNTP) { // If a minute has passed since last NTP request
+uint32_t checkNTPsync(unsigned long currentMillis, int interval = intervalNTP){
+  if (currentMillis - prevNTP > intervalNTP) {  // If a minute has passed since last NTP request
     prevNTP = currentMillis;
     Serial.println("\r\nSending NTP request ...");
     sendNTPpacket(timeServerIP);               // Send an NTP request
-  }
-
-  uint32_t time = getTime();                   // Check if an NTP response has arrived and get the (UNIX) time
-  if (time) {                                  // If a new timestamp has been received
-      timeUNIX = time;
-      Serial.print("NTP response:\t");
-      Serial.println(timeUNIX);
-      lastNTPResponse = currentMillis;
+    return checkNTPtime();
+  //uint32_t time = getTime();                   // Check if an NTP response has arrived and get the (UNIX) time
+  //if (time) {                                  // If a new timestamp has been received
+  //    timeUNIX = time;
+  //    Serial.print("NTP response:\t");
+  //    Serial.println(timeUNIX);
+  //    lastNTPResponse = currentMillis;
     } else if ((currentMillis - lastNTPResponse) > 3600000) {
         Serial.println("More than 1 hour since last NTP response. Rebooting.");
         Serial.flush();
         ESP.reset();
       }
+}
 
+void loop() {
+  unsigned long currentMillis = millis();       // get uptime since loop start
+  timeUNIX = checkNTPsync(currentMillis);
   uint32_t actualTime = timeUNIX + (currentMillis - lastNTPResponse)/1000;
   uint32_t futureTime = actualTime + 300; // + incrementSeconds
   if (actualTime != prevActualTime && timeUNIX != 0) { // If a second has passed since last print
